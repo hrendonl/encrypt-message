1f7b8ed98fd4dc7689a8b179c9e079ef3dafeacd
Hugo Camilo Rendon Lozano <hrvega14@hotmail.com>
2024-11-12 02:26:28 -0500

    change: design and fields border focus

--- src/pages/Home/components/Content/components/components/components/MessageBox/index.jsx
+++ src/pages/Home/components/Content/components/components/components/MessageBox/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import "./MessageBox.css";
+import { useState } from "react";
 
 export const MessageBox = ({
   tabActivated,
@@ -9,6 +10,8 @@ export const MessageBox = ({
   resetProcessedMessage,
 }) => {
   const nameTextarea = "message";
+
+  const [isFocus, setIsFocus] = useState(false)
 
   const onTextareaChange = (e) => {
     onMessageChange(e);
@@ -40,12 +43,16 @@ export const MessageBox = ({
           )}
         </div>
       </div>
-      <textarea
-        name={nameTextarea}
-        onChange={onTextareaChange}
-        placeholder="Escribe el mensaje que deseas cifrar o descifrar"
-        value={message}
-      />
+      <div className={`textarea-container ${isFocus && "textarea-focus"}`}>
+        <textarea
+          onFocus={()=>setIsFocus(true)}
+          onBlur={()=>setIsFocus(false)}
+          name={nameTextarea}
+          onChange={onTextareaChange}
+          placeholder="Escribe el mensaje que deseas cifrar o descifrar"
+          value={message}
+        />
+      </div>
       <div className="buttons-actions">
         <button type="submit" className="button-secondary">
           <i className="bi bi-unlock-fill"></i>
