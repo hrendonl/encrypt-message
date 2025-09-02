7594ecc162063b8b0f677f2ba020508ca4bb941a
Hugo Camilo Rendon Lozano <hrvega14@hotmail.com>
2025-09-02 01:17:16 -0500

    Refactor: calculateKeyNumber

--- src/helpers/calculateKeyNumber.js
+++ src/helpers/calculateKeyNumber.js
@@ -1,7 +1,7 @@
 export const calculateKeyNumber = (keyword, CHARSET_NUMBERS) => {
     let numberKey = 0;
     Array.from(keyword).forEach((letter) => {
-      numberKey += ALPHABET_NUMBERS[letter];
+      numberKey += CHARSET_NUMBERS[letter];
     });
   return numberKey
 }
